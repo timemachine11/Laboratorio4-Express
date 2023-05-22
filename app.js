88913803dd51ef0a146359db9f097585f8feb53c
Silvester <silvester0620@gmail.com>
2023-05-22 11:42:20 -0500

    Segundo commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 
 // 5
 let {usuarios} = require("./models/usuario")
+let {productos} = require('./models/producto')
 
 // 9 middleware 
 app.use(express.json())
@@ -64,7 +65,107 @@ app.delete("/usuarios/:id", (req, res) => {
         return res.status(404).json({status: "El usuario no se encuentra"})
 })
 
-// 6
+/**
+ * parte del punto 12 en adelante...
+ */
+
+// 7
+app.get('/productos', (req, res) => {
+    res.json(productos)
+})
+
+// 8
+// app.get('/productos/:id', (req, res) => {
+//     const id_producto = req.params.id
+//     const filtro = productos.filter((producto) => producto.id_producto == id_producto)
+
+//     if (filtro.length > 0) {
+//         return res.json(filtro)
+
+//     } else {
+//         return res.status(404).json({status: "El producto no esta disponible..."})
+//     }
+// })
+
+// 9
+app.post('/productos', (req, res) => {
+    let body = req.body
+    body.id_producto = productos.length+1
+    productos.push(body)
+
+    return res.status(201).json(body)
+})
+
+// 10
+app.put('/productos/:id', (req, res) => {
+    let id_producto = req.params.id
+    let body = req.body
+
+    const filtro = productos.filter((producto) => producto.id_producto == id_producto)
+
+    if (filtro.length > 0) {
+        productos = productos.filter((producto) => producto.id_producto != id_producto)
+        productos.push(body)
+
+        return res.status(201).json(body)
+
+    } else {
+        return res.status(201).json({status: 'El producto no existe'})
+    }
+})
+
+// 11
+app.delete('/productos/:id', (req, res) => {
+    let id_producto = req.params.id
+    const filtro = productos.filter((producto) => producto.id_producto == id_producto)
+
+    if (filtro.length > 0) {
+        productos = productos.filter((producto) => producto.id_producto != id_producto)
+
+        return res.status(200).json(filtro)
+
+    } else
+        return res.status(404).json({status: 'El producto no esta disponible'})
+})
+
+// 13
+// app.get('/productos/:marca', (req, res) => {
+//     const marca = req.params.marca
+//     const filtro = productos.filter((producto) => producto.marca == marca)
+
+//     if (filtro.length > 0)
+//         return res.status(200).json(filtro)
+
+//     else
+//         return res.status(404).json({status: 'El producto no se encuentra'})
+// })
+
+// 14
+// app.get('/productos/:precio', (req, res) => {
+//     const precio = req.params.precio
+//     const filter = productos.filter((producto) => producto.precio >= precio)
+
+//     if (filter.length > 0)
+//         return res.status(200).json(filter)
+
+//     else
+//         return res.status(404).json({status: 'No hay producto scon ese precio'})
+// })
+
+// 15
+app.get('/productos/:precio', (req, res) => {
+    const precio = req.params.precio
+    const filter = productos.filter((producto) => producto.precio <= precio)
+
+    if (filter.length > 0)
+        return res.status(200).json(filter)
+
+    else
+        return res.status(404).json({status: 'No hay producto scon ese precio'})
+})
+
+
+
 app.listen(8080, () => {
-    console.log("Se ha levantado el servidor...");
+    console.log("Se ha levantado el servidor 8080...");
 })
